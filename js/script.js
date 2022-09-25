4e7684f2f93d5346c7f9b53c466eb7d255e7bdac
bzr.lipe <bzr.lipe@gmail.com>
2022-09-25 20:15:38 -0300

    page3 mobile ok

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const btnMobile = document.getElementById('checkbox-menu');    
 
 function toggleMenu() {
+    if (event.type === 'touchstart') event.preventDefault;
     const nav = document.getElementById('nav');    
     nav.classList.toggle('active');
 
 }
 
-btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
